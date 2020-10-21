79f37b9abf27a20d27c25282cf9d02d730352dae
Aditya Pulakuniyil <70507265+Exch0o@users.noreply.github.com>
2020-10-21 00:25:14 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+choice = input("""Type 1 for a Gift, Type 2 for a surprise, Type for free fortnite Vbucks link: """)
+
+if choice == '1':
+    print("here take a gift")
+
+elif choice == '2':
+    print("Surprise!")
+
+elif choice == '3':
+    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab")
